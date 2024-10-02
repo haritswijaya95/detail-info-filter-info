fbdf6ce61e430c2016432e78290e968a57362de5
haritswijaya95 <155189685+haritswijaya95@users.noreply.github.com>
2024-10-02 02:56:15 -0700

    Add files via upload

---
+++ style css filter cfest-info/cfest-info/config/config.js
@@ -0,0 +1,9 @@
+
+
+export const API_BASE_URL = 'https://smoothly-tolerant-beagle.ngrok-free.app'
+
+export const cfest = {
+  events_data:'/events/events-data',
+  events_detail:'/events/events-detail',
+  events_articles:'/events/articles'
+}
